d86617a9281b97091732fd735c15011aba1e8835
Nikki <nikki.satopdx@gmail.com>
2020-01-28 16:56:20 -0800

    props are being passed correctly now

--- src/components/Character.js
+++ src/components/Character.js
@@ -9,56 +9,20 @@ export default class Character extends Component{
     name: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired 
+    
   }
 
   render() {
-    const  { image, location } = this.props;
+    const  { name, species, image, origin, location } = this.props;
     return (
       <>
        
-
-        <div className={styles.container}>
-          <div className={styles.card}>
-            <img src="./assets/baby-yoda1.jpeg"></img>
-            <dd>Location: Dessert</dd>
-          </div>
-
-          <div className={styles.card}>
-            <img className= "image" src="./assets/baby-yoda-walk.jpg"></img>
-            <dd>Location: board walk</dd>
-          </div>
-
-          <div className={styles.card}>
-            <img className= "image" src="./assets/baby-yoda-tea.jpeg"></img>
-            <dd>Location: cuppa tea</dd>
-          </div>
-          
-          <div>
-            <img className= "image" src="./assets/baby-yoda-stick.jpg"></img>
-            <dd>Location: Spaceship</dd>
-          </div>
-
-          <div>
-            <img className= "image" src="./assets/baby-yoda-saber.jpg"></img>
-            <dd>Location: Unknown</dd>
-          </div>
-
-          <div>
-            <img className= "image" src="./assets/baby-yoda-guy.jpg"></img>
-            <dd>Location: Diners Drive-ins and dives</dd>
-          </div>
-
-
-          <div>
-            <img className= "image" src="./assets/baby-yoda-gear.jpg"></img>
-            <dd>Location: Spaceship</dd>
-          </div>
-
-          <div>
-            <img className= "image" src="./assets/baby-yoda-force.png"></img>
-            <dd>Location: Egg</dd>
-          </div>
-
+        <div className={styles.card}>
+          <img src={image}></img>
+          <h1>Name: {name}</h1>
+          <h2>Species: {species}</h2>
+          <h2>Origin: {origin}</h2>
+          <h2>Location: {location}</h2>
         </div>
       </>
     );
